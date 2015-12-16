60b91f9adcc757f9be227310941db394a1b75e89
Christian Holl <cyborgx1@gmail.com>
2015-12-16 09:24:15 +0100

    fix plugin macro not being there

--- h4r_ev3_sensor_control/src/ev3_ultrasonic_range_controller.cpp
+++ h4r_ev3_sensor_control/src/ev3_ultrasonic_range_controller.cpp
@@ -24,4 +24,7 @@ Ev3UltraSonicRangeController::~Ev3UltraSonicRangeController()
 	// TODO Auto-generated destructor stub
 }
 
+
+PLUGINLIB_EXPORT_CLASS(h4r_ev3_sensor_control::Ev3UltraSonicRangeController, controller_interface::ControllerBase)
+
 } /* namespace h4r_ev3_ctrl */
